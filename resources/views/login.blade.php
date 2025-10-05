0b416ac29217a1ff8daf594afabe1ee5881b72c9
Jesil Jose <mailstojesil@gmail.com>
2025-10-05 20:24:10 +0530

    Login blades improved

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,14 +1,31 @@
 @extends('layouts.master')
 
-@section('nav')
-    <nav class="navbar navbar-expand-lg navbar-light bg-light nav-pad">
-        <a class="navbar-brand" href="{{ url('/') }}"><h4>Laragram</h4></a>
-        <div class="ms-auto">
-             <a href="{{ url('login') }}">Login</a>
-        </div>
-    </nav>
-@endsection
-
 @section('content')
-    <login></login>
+    <div class="auth-wrapper">
+        <div class="auth-card">
+            <div class="auth-icon">
+                <i class="fa-solid fa-mobile-screen-button"></i>
+            </div>
+            <h2>Sign In to Telegram</h2>
+
+            <form method="POST" action="{{ route('auth.login.check') }}" class="auth-form">
+                @csrf
+                <div class="form-group">
+                    <label for="phone" class="form-label mb-1">Phone Number</label>
+                    <input id="phone" type="text" class="form-control" name="phone" required autofocus placeholder="+1234567890">
+                </div>
+
+                <div class="form-group mt-4">
+                    <button type="submit" class="btn btn-primary">
+                        Send Code
+                    </button>
+                </div>
+            </form>
+
+            <p class="auth-note">
+                Please enter your number in the international format.<br>
+                (e.g., `+91` for India, `+1` for USA)
+            </p>
+        </div>
+    </div>
 @endsection
